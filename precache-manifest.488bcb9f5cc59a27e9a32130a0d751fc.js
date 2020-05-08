01f98162e38cd7c9f8b55919bfcba71181136822
emackinnon1 <emackinnon100390@gmail.com>
2020-05-07 19:45:23 -0600

    Updates

---
+++ precache-manifest.488bcb9f5cc59a27e9a32130a0d751fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9194fbea13ad8119e841ac01528c281",
+    "url": "/whats-new/index.html"
+  },
+  {
+    "revision": "af3a9d7f33bb03e511ca",
+    "url": "/whats-new/static/css/main.92fd0c7a.chunk.css"
+  },
+  {
+    "revision": "16a513724fa1acd520a7",
+    "url": "/whats-new/static/js/2.ed94ee19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whats-new/static/js/2.ed94ee19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3a9d7f33bb03e511ca",
+    "url": "/whats-new/static/js/main.661772d1.chunk.js"
+  },
+  {
+    "revision": "deccfd15b8e90df9b470",
+    "url": "/whats-new/static/js/runtime-main.fe69efb1.js"
+  }
+]);
