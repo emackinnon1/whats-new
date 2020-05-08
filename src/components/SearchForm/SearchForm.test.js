3ef138be6d754acfdb461a1690c718afda333e47
emackinnon1 <emackinnon100390@gmail.com>
2020-05-07 19:34:05 -0600

    Correct formatting in some tests

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import SearchForm from "./SearchForm";
+
 describe("SearchForm", () => {
 	it("When search term is entered, it should filter articles by search term", () => {
 		const { debug, getByText, getByPlaceholderText } = render(<SearchForm />);
